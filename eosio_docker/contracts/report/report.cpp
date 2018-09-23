597bbbd118d8d92417e1c5a78d82c0a4208816ac
Johannes <johanneshage97@gmail.com>
2018-09-23 12:30:03 +0100

    deleted unnecessary smart contracts of the boilerplates

--- eosio_docker/contracts/report/report.cpp
+++ eosio_docker/contracts/report/report.cpp
@@ -3,18 +3,7 @@
 using namespace eosio;
 
 // Smart Contract Name: report
-// Table struct:
-//   notestruct: multi index table to store the notes
-//     prim_key(uint64): primary key
-//     user(account_name/uint64): account name for the user
-//     note(string): the note message
-//     timestamp(uint64): the store the last update block time
-// Public method:
-//   isnewuser => to check if the given account name has note in table or not
-// Public actions:
-//   update => put the note into the multi-index table and sign by the given account
 
-// Replace the contract class name when you start your own project
 class report : public eosio::contract
 {
   private:
